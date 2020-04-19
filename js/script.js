859d3eda4081a7681a1fae2afbb78ba65b6d6bbe
Slimakanzer <gleb.larochkin@gmail.com>
2020-04-19 12:45:22 +0300

    fixed security layer block

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
-$.getJSON('http://ipinfo.io', function (data) {
-
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://whoami-65bb.restdb.io/rest/ip-info",
-        "method": "POST",
-        "headers": {
-            "content-type": "application/json",
-            "x-apikey": "02cfc574ed5f7a7768ee123e8b5aa09ec3166",
-            "cache-control": "no-cache"
-        },
-        "processData": false,
-        "data": JSON.stringify(data)
-    }
-
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
+$.getJSON('https://ipinfo.io', function (data) {
+    sendIp(data);
 });
 
 function sendIp(datajson) {
