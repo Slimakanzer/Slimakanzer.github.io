4ca5d55ece47109cb44404693e67cffe74e7bc20
Slimakanzer <gleb.larochkin@gmail.com>
2020-04-19 12:39:12 +0300

    ip logger

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$.getJSON('http://ipinfo.io', function (data) {
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://whoami-65bb.restdb.io/rest/ip-info",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+            "x-apikey": "02cfc574ed5f7a7768ee123e8b5aa09ec3166",
+            "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify(data)
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+});
+
+function sendIp(datajson) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://whoami-65bb.restdb.io/rest/ip-info",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+            "x-apikey": "02cfc574ed5f7a7768ee123e8b5aa09ec3166",
+            "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify(datajson)
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
