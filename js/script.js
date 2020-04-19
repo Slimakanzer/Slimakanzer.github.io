4460887af4279e11dfe93bdc027cd1cee05d639c
Slimakanzer <gleb.larochkin@gmail.com>
2020-04-19 13:15:06 +0300

    added cors api key

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
+var corsApiKey = "5e9c2029436377171a0c259f";
+
 $.getJSON('https://ipinfo.io', function (data) {
     sendIp(data);
 });
 
 function sendIp(datajson) {
+    var jsondata = {"ip": datajson};
     var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://whoami-65bb.restdb.io/rest/ip-info",
-        "method": "POST",
-        "headers": {
-            "content-type": "application/json",
-            "x-apikey": "02cfc574ed5f7a7768ee123e8b5aa09ec3166",
-            "cache-control": "no-cache"
-        },
-        "processData": false,
-        "data": JSON.stringify(datajson)
+      "async": true,
+      "crossDomain": true,
+      "url": "https://whoami-65bb.restdb.io/rest/ip-info",
+      "method": "POST",
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": corsApiKey,
+        "cache-control": "no-cache"
+      },
+      "processData": false,
+      "data": JSON.stringify(jsondata)
     }
-
+    
     $.ajax(settings).done(function (response) {
-        console.log(response);
+      console.log(response);
     });
 }
